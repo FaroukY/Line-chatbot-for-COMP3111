0e4ea60270ea47ec93a6d7e0864adc9d12bbe652
FaroukY <af.y.f@hotmail.com>
2017-09-30 23:33:38 -0400

    Finished lab 3, connected to postgrsql database

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/SQLDatabaseEngine.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/SQLDatabaseEngine.java
@@ -5,24 +5,40 @@ import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 import java.sql.*;
 import java.net.URISyntaxException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URI;
 
 @Slf4j
 public class SQLDatabaseEngine extends DatabaseEngine {
 	@Override
 	String search(String text) throws Exception {
-		//Write your code here
-		return null;
+		Connection con = this.getConnection();
+		log.info("About to execute query");
+
+		PreparedStatement stmt = con.prepareStatement("SELECT * from wordlist");
+		ResultSet rs = stmt.executeQuery();
+		while(rs.next()) {
+			String word = rs.getString(2);
+			String reply = rs.getString(3);
+			if (text.toLowerCase().contains(word.toLowerCase())) {
+				return reply;
+			}
+		}
+		throw new Exception("NOT FOUND");
 	}
 	
 	
 	private Connection getConnection() throws URISyntaxException, SQLException {
 		Connection connection;
+
 		URI dbUri = new URI(System.getenv("DATABASE_URL"));
 
 		String username = dbUri.getUserInfo().split(":")[0];
 		String password = dbUri.getUserInfo().split(":")[1];
 		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() +  "?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
+
 
 		log.info("Username: {} Password: {}", username, password);
 		log.info ("dbUrl: {}", dbUrl);
